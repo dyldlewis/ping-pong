dd9d8c36da51a2c9591d103dc0798ca44f1a6539
Dylan Lewis <dyldlewis@gmail.com>
2017-06-09 10:33:39 -0300

    add function to check for non-numberical items

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,26 @@
+function numCheck(number) {
+  var result = ""
+  if (isNaN(number)) {
+    result = "Please enter a valid number";
+  };
+  return result;
+};
+
+
+
+
+
+
+
+// front end logic //
+
 $(document).ready(function() {
   $("#pong").submit(function(event) {
     event.preventDefault();
     var number = parseInt($("input#number-string").val());
-    console.log(number)
+    console.log(number);
+    var check = numCheck(number);
+    $("#result").text(check);
+
   });
 });
