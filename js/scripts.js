12018a42f3b355f908a92b8af8085e5fee378e35
Dylan Lewis <dyldlewis@gmail.com>
2017-06-09 12:32:52 -0300

    combine numCheck function into counter function

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,29 @@
 function numCheck(number) {
   var result = ""
   if (isNaN(number)) {
-    result = "Please enter a valid number";
+    alert("Please enter a valid number");
   };
   return result;
 };
 
 function counter(num) {
   var result = []
-  for (i = 1; i <= num; i += 1) {
-    if (i % 15 === 0) {
-    result.push("pingpong")
-  } else if (i % 3 == 0) {
-    result.push("ping")
-  } else if (i % 5 === 0) {
-    result.push("pong")
+  if (isNaN(num)) {
+    alert("Please enter a valid number");
   } else {
-    result.push(i)
-  }
-  };
+
+      for (i = 1; i <= num; i += 1) {
+        if (i % 15 === 0) {
+        result.push("pingpong")
+      } else if (i % 3 == 0) {
+        result.push("ping")
+      } else if (i % 5 === 0) {
+        result.push("pong")
+      } else {
+        result.push(i)
+      }
+      };
+    }
   console.log(result)
   return result;
 };
